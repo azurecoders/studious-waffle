635eb803a804cbbf2b119d770a335f1f6c6d7cfc
Muzammil Ahmed <166981810+azurecoders@users.noreply.github.com>
2025-01-21 05:08:55 +0500

    Fixed db.ts and eslint issues

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -11,8 +11,8 @@ if (!DATABASE_URL) {
 // Extend the global object to include mongoose caching
 declare global {
   var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
+    conn: mongoose.Connection | null;
+    promise: Promise<mongoose.Connection> | null;
   };
 }
 
@@ -36,9 +36,7 @@ async function connectDB() {
 
     cached.promise = mongoose
       .connect(DATABASE_URL!, opts)
-      .then((connection) => {
-        return connection;
-      });
+      .then((connection) => connection.connection); // Use `connection.connection` for the Mongoose connection
   }
   cached.conn = await cached.promise;
   return cached.conn;
